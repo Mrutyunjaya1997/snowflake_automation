51561c679f64a0212d6cfffeb4a62f351f35ee32
Mrutyunjaya Sahoo <Mrutyunjaya.Sahoo@novigo.com>
2025-01-20 15:50:52 +0530

    vesion two script added to work with new look up table

---
+++ bronze_silver/bronze_silver_mergequery.py
@@ -0,0 +1,3 @@
+# 1. when the parent table gets populated relevant dimension table should be populated.
+# 2. Fact table should be fetching correct ids from the dimension tables based on the parent table.
+# 3. audit columns should be handled(insert flag, is data transferred)
